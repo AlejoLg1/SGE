d228acb65bf1904efe23609fdf33abe0ce3de2d8
AlejoLg1 <legname.alejo@gmail.com>
2024-04-29 20:26:06 -0300

    Append Administradores Class to main

--- sistema_gestion/main.cpp
+++ sistema_gestion/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
+#include "Administrador.h"
 
 using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Spanish");
-
-    cout << "hola mundo!" << endl;
 
     return 0;
 }
